213e794da9d0e84ae1d22a5ca5686669517e198b
Юрий Кубасов <kubasovurij93@gmail.com>
2024-10-28 13:32:39 +0300

    2-nd try

--- PythonSQLite.py
+++ PythonSQLite.py
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS Students (
     st_id INTEGER PRIMARY KEY AUTOINCREMENT, 
     st_name TEXT NOT NULL,
     st_surname TEXT NOT NULL,
-    st_date_of_birth DATE NOT NULL,
+    st_date_of_birth TEXT NOT NULL,
     st_department TEXT NOT NULL
     )
 """
@@ -58,6 +58,7 @@ CREATE TABLE IF NOT EXISTS Grades (
     gr_id INTEGER PRIMARY KEY AUTOINCREMENT, 
     st_id INT,
     ex_id INT,
+    gr_score INT,
     FOREIGN KEY (st_id) REFERENCES Students(st_id),
     FOREIGN KEY (ex_id) REFERENCES Exams(ex_id)
     )
@@ -65,7 +66,7 @@ CREATE TABLE IF NOT EXISTS Grades (
 cursor.execute(create_table_query)
 print("Таблица 'Grades' успешно создана.")
 
-# Шаг 2: Вставка данных
+
 def add_student(name, surname, department, date_of_birth):
     cursor.execute('INSERT INTO Students (st_name, st_surname, st_department, st_date_of_birth) VALUES (?, ?, ?, ?)', (name, surname, department, date_of_birth))
     db_connection.commit()
@@ -77,6 +78,8 @@ def add_teacher(name, surname, department):
 def add_course(title, description, teacher_id):
     cursor.execute('INSERT INTO Courses (co_title, co_description, te_id) VALUES (?, ?, ?)', (title, description, teacher_id))
     db_connection.commit()
+
+
 def update_student(student_id, name=None, surname=None, date_of_birth=None):
     if name:
         cursor.execute("UPDATE Students SET st_name = ? WHERE st_id = ?", (name, student_id))
@@ -85,6 +88,7 @@ def update_student(student_id, name=None, surname=None, date_of_birth=None):
     if date_of_birth:
         cursor.execute("UPDATE Students SET st_date_of_birth = ? WHERE st_id = ?", (date_of_birth, student_id))
     db_connection.commit()
+
 def update_teacher(te_id, name=None, surname=None, date_of_birth=None):
     if name:
         cursor.execute("UPDATE Teachers SET te_name = ? WHERE te_id = ?", (name, te_id))
@@ -94,14 +98,86 @@ def update_teacher(te_id, name=None, surname=None, date_of_birth=None):
         cursor.execute("UPDATE Students SET te_department = ? WHERE te_id = ?", (date_of_birth, te_id))
     db_connection.commit()
 
+def update_cource(co_id, title=None, description=None, te_id=None):
+    if name:
+        cursor.execute("UPDATE Cources SET co_title = ? WHERE co_id = ?", (title, co_id))
+    if surname:
+        cursor.execute("UPDATE Cources SET co_description = ? WHERE co_id = ?", (description, co_id))
+    if date_of_birth:
+        cursor.execute("UPDATE Couces SET te_id = ? WHERE Co_id = ?", (te_id, co_id))
+    db_connection.commit()
+
+
+def delete_student(student_id):
+    cursor.execute('DELETE FROM Students WHERE st_id ='+student_id)
+
+def delete_teacher(teacher_id):
+    cursor.execute('DELETE FROM Teachers WHERE te_id ='+teacher_id)
+
+def delete_cource(cource_id):
+    cursor.execute('DELETE FROM Cources WHERE co_id ='+cource_id)
+
+
+def get_students_by_department(department):
+    cursor.execute(f"SELECT st_id, st_name, st_surname FROM Students WHERE st_department = '{department}'")
+    result = cursor.fetchall()
+    print(f'Факультет "{department}"')
+    for row in result:
+        print(f'ID:{row[0]}----Имя:{row[1]}----Фамилия:{row[2]}')
+    print('\n')
+
+def get_teacher_courses(teacher_id):
+    cursor.execute(f"SELECT co_id, co_title, co_description FROM Courses WHERE te_id = {teacher_id}")
+    result = cursor.fetchall()
+    print(f'Преподаватель с ID {teacher_id}')
+    for row in result:
+        print(f"ID курса:{row[0]}----Название:{row[1]}----Описание:{row[2]}")
+    print("\n")
+
+def get_grades(student_id):
+    cursor.execute(f"SELECT ex_id, gr_score FROM Grades WHERE st_id = {student_id}")
+    result = cursor.fetchall()
+    print(f"Успеваемость студента с ID {student_id}")
+    for row in result:
+        print(f"ID экзамена:{row[0]}----Оценка:{row[1]}")
+    print("\n")
+
+
+def average_student(student_id):
+    cursor.execute(f"SELECT avg(gr_score) FROM Grades WHERE st_id = {student_id}")
+    res = cursor.fetchall()
+    print(f"Средний балл студента с ID {student_id}: {res[0][0]}\n")
+
+def average_of_department(department):
+    cursor.execute(f"""SELECT avg(gr_score)
+        FROM Students s
+        JOIN Grades g on s.id = g.student_id
+        WHERE department = '{department}'""")
+    res = cursor.fetchall()
+    print(f"Средний балл по факультету {department}: {res[0][0]}\n")
 
 while True:
+    # try:
+
     print("Выберите действие:")
     print("1. Добавить нового студента")
     print("2. Добавить нового преподавателя")
     print("3. Добавить новый курс")
+    print()
     print('4. Изменить данные о студенте')
     print('5. Изменить данные об учителе')
+    print('6. Изменить данные о курсе')
+    print()
+    print('7. Удалить студента')
+    print('8. Удалить преподавателя')
+    print('9. Удалить курс')
+    print()
+    print('10. Вывод списка студентов по курсу')
+    print('11. Вывод курса преподавателя')
+    print('12. Вывод оценок студента')
+    print()
+    print('13. Расчет среднего балла студента')
+    print('14. Расчет среднего балла по курсу')
 
 
     choice = input("Введите номер действия (или 'exit' для выхода): ")
@@ -124,69 +200,78 @@ while True:
         description = input("Введите описание курса: ")
         teacher_id = int(input("Введите ID преподавателя курса: "))
         add_course(title, description, teacher_id)
-   
+
     elif choice == '4':
         choice1 = input('Введите тип данных для обновления (name, surname, date_of_birth) or exit ')
         while choice1 != 'exit':
             student_id = int(input('Введите id студента '))
             if choice1 == 'name':
                 name = input('Введите новое имя ')
-                update_student(student_id, name)
+                update_student(name, student_id)
             elif choice1 == 'surname':
                 surname = input('Введите новую фамилию ')
-                update_student(student_id, surname)
+                update_student(surname, student_id)
             elif choice1 == 'date_of_birth':
                 date_of_birth = input('Введите новую дату ')
-                update_student(student_id, date_of_birth)
+                update_student(date_of_birth, student_id)
             choice1 = input('Введите тип данных для обновления (name, surname, date_of_birth) or exit ')
     elif choice == '5':
         choice1 = input('Введите тип данных для обновления (name, surname, department) or exit ')
         while choice1 != 'exit':
-            student_id = int(input('Введите id учителя'))
+            te_id = int(input('Введите id учителя'))
             if choice1 == 'name':
                 name = input('Введите новое имя')
-                update_student(student_id, name)
+                update_student(name, te_id)
             elif choice1 == 'surname':
                 surname = input('Введите новую фамилию')
-                update_student(student_id, surname)
+                update_student(surname, te_id )
+            elif choice1 == 'department':
+                department = input('Введите новый факультет')
+                update_student( department, te_id)
+            choice1 = input('Введите тип данных для обновления (name, surname, department) or exit ')
+    elif choice == '6':
+        choice1 = input('Введите тип данных для обновления (title, description, teacher_id) or exit ')
+        while choice1 != 'exit': 
+            co_id = int(input('Введите id курса '))
+            if choice1 == 'title':
+                title = input('Введите новое название: ')
+                update_student(title, co_id )
+            elif choice1 == 'description':
+                description = input('Введите новое описание: ')
+                update_student(description, co_id)
             elif choice1 == 'date_of_birth':
-                date_of_birth = input('Введите новый факультет')
-                update_student(student_id, date_of_birth)
-    choice1 = input('Введите тип данных для обновления (name, surname, department) or exit ')
-db_connection.close()
+                te_id = input('Введите id нового преподавателя: ')
+                update_student(te_id, co_id)
+            choice1 = input('Введите тип данных для обновления (title, description, teacher_id) or exit ')
 
-# Шаг 3: Выборка данных
-select_query = "SELECT * FROM Users"
-cursor.execute(select_query)
-result = cursor.fetchall()
-print("\nДанные в таблице 'Users':")
-for row in result:
-    print(f"ID: {row[0]}, Имя: {row[1]}, Возраст: {row[2]}")
+    elif choice == '7':
+        student_id = input('Введите id студента: ')
+        delete_student(student_id)
+    elif choice == '8':
+        teacher_id = input('Введите id учителя: ')
+        delete_student(teacher_id)
+    elif choice == '9':
+        cource_id = input('Введите id курса: ')
+        delete_student(cource_id)
+    
+    elif choice =='10':
+        department = input("Введите факультет: ")
+        student = get_students_by_department(department)
+    elif choice == '11':
+        teacher_id = int(input("ID преподавателя: "))
+        get_teacher_courses(teacher_id)
+    elif choice == '12':
+        student_id = int(input("ID студента: "))
+        get_grades(student_id)
 
-# Шаг 4: Обновление данных
+    elif choice == '13':
+        student_id = int(input("ID студента: "))
+        average_student(student_id)
+    elif choice == '14':
+        department = input("Название факультета: ")
+        average_of_department(department)
+# except:
+        # print("ошибка! введи еще раз\n")
 
-# Шаг 5: Выборка данных после обновления
-cursor.execute(select_query)
-result = cursor.fetchall()
-print("\nДанные в таблице 'Users' после обновления:")
-for row in result:
-    print(f"ID: {row[0]}, Имя: {row[1]}, Возраст: {row[2]}")
-
-# Шаг 6: Удаление данных
-delete_query = "DELETE FROM Users WHERE name = ?"
-user_to_delete = ("Alice Johnson",)
-cursor.execute(delete_query, user_to_delete)
-db_connection.commit()
-print(f"Пользователь 'Alice Johnson' успешно удален.")
-
-# Шаг 7: Окончательная выборка данных после удаления
-cursor.execute(select_query)
-result = cursor.fetchall()
-print("\nДанные в таблице 'Users' после удаления:")
-for row in result:
-    print(f"ID: {row[0]}, Имя: {row[1]}, Возраст: {row[2]}")
-
-# Закрытие соединения
-cursor.close()
 db_connection.close()
 print("\nСоединение с базой данных закрыто.")
